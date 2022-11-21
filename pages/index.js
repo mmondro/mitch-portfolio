c9dd5fe3841b1a96ff055d8cc1f386be44e0b062
Mitchell Mondro <35818209+mmondro@users.noreply.github.com>
2022-11-21 12:52:33 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import {
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import deved from "../public/dev-ed-wave.png";
+import deved from "../public/Mitch.png";
 import code from "../public/code.png";
 import design from "../public/design.png";
 import consulting from "../public/consulting.png";
@@ -31,7 +31,7 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyed</h1>
+            <h1 className="font-burtons text-xl">Mitch's Portfolio</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -42,8 +42,9 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
+                  href="Mitchell Mondro Resume November 2018 emm.pdf"
                 >
+                
                   Resume
                 </a>
               </li>
@@ -51,19 +52,24 @@ export default function Home() {
           </nav>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Dimitri Marco
+              Mitch Mondro
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Developer and designer.
+              Marketing Expert
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Freelancer providing services for programming and design content
-              needs. Join me down below and let's get cracking!
+            This is a test.
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <AiFillTwitterCircle />
-              <AiFillLinkedin />
-              <AiFillYoutube />
+              
+              <a href="https://twitter.com/mitchellmondro" target="_blank" rel="noopener noreferrer">
+              <AiFillTwitterCircle /> </a>
+              <a href="https://www.linkedin.com/in/mitchellmondro/" target="_blank" rel="noopener noreferrer">
+              <AiFillLinkedin /> </a>
+              <a href="https://www.youtube.com/channel/UCNtjzKme85NQ4xfAZWK3wLw" target="_blank" rel="noopener noreferrer">
+              <AiFillYoutube /> </a>
+              
+      
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
               <Image src={deved} layout="fill" objectFit="cover" />
