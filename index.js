0a514b5a557b9eddab0c4bf6e359b8026700d816
Mitch <mmondro@clarku.edu>
2022-11-22 21:11:56 -0500

    Test

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ export default function Home() {
               Marketing Expert
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-            I am a marketing professional interested in the business applications of blockchain technology.
+            Blockchain
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               
